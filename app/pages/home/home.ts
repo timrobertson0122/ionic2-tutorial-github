d47127a7069a14b9b546d7ce1f552a87ce939fdf
Tim <timrobertson0122@gmail.com>
2016-04-15 20:24:39 +0100

    repo list and details page with readme info

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,10 +1,29 @@
-import {Page} from 'ionic-angular';
+import {Page, NavController} from 'ionic-angular';
+import {GitHubService} from '../../services/github';
+import {DetailsPage} from '../details/details';
 
 @Page({
-  templateUrl: 'build/pages/home/home.html'
+  templateUrl: 'build/pages/home/home.html',
+  providers: [GitHubService]
 })
 export class HomePage {
-  constructor() {
+  public foundRepos;
+  public username;
 
+  constructor(private github: GitHubService,
+              private nav: NavController) {
+  }
+  goToDetails(repo) {
+    this.nav.push(DetailsPage, { repo: repo });
+  }
+
+  getRepos() {
+    this.github.getRepos(this.username).subscribe(
+      data => {
+        this.foundRepos = data.json();
+      },
+      err => console.error(err),
+      () => console.log('getRepos Completed')
+    );
   }
 }
